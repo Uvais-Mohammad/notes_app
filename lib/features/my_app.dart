11703c7ed76495796dea4adb9226c6b55897186d
Uvais-Mohammad <uvaiskc55@gmail.com>
2023-12-23 10:33:41 +0530

    Update theme and styles in MyApp

--- lib/features/my_app.dart
+++ lib/features/my_app.dart
@@ -6,8 +6,43 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: NotesScreen(),
+    return MaterialApp(
+      home: const NotesScreen(),
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+        appBarTheme: const AppBarTheme(
+          centerTitle: false,
+          titleTextStyle: TextStyle(
+            fontSize: 28,
+            fontFamily: 'Poppins',
+            color: Colors.black,
+          ),
+        ),
+        listTileTheme: const ListTileThemeData(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+          ),
+          contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 9),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          border: InputBorder.none,
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+          ),
+          bodyLarge: TextStyle(
+            fontSize: 16,
+            color: Colors.black,
+          ),
+          bodySmall: TextStyle(
+            fontSize: 13,
+            color: Colors.black,
+          ),
+        ),
+      ),
     );
   }
 }
